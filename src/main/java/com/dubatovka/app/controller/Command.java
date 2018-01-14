0754310704dcc19117ce743b4d89b46000f48916
vadimdubka <vadimdubka@gmail.com>
2018-01-14 23:06:30 +0300

    Default method for validation request parameters is added in Command interface.

--- src/main/java/com/dubatovka/app/controller/Command.java
+++ src/main/java/com/dubatovka/app/controller/Command.java
@@ -1,7 +1,20 @@
 package com.dubatovka.app.controller;
 
+import com.dubatovka.app.service.ValidatorService;
+import com.dubatovka.app.service.impl.ServiceFactory;
+
 import javax.servlet.http.HttpServletRequest;
+
+import static com.dubatovka.app.manager.ConfigConstant.MESSAGE_INVALID_REQUEST_PARAMETER;
+import static com.dubatovka.app.manager.ConfigConstant.MESSAGE_SEPARATOR;
 
 public interface Command {
     PageNavigator execute(HttpServletRequest request);
+    
+    default void validateRequestParams(StringBuilder errorMessage, String... params) {
+        ValidatorService validatorService = ServiceFactory.getValidatorService();
+        if (!validatorService.isValidRequestParam(params)) {
+            errorMessage.append(MESSAGE_INVALID_REQUEST_PARAMETER).append(MESSAGE_SEPARATOR);
+        }
+    }
 }
